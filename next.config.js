5e859f05750b8c6642729750d4a0388d989cc828
Hiroya Shitogiden <dende.capo@gmail.com>
2023-07-12 16:01:28 +0900

    リダイレクト追加

--- next.config.js
+++ next.config.js
@@ -6,5 +6,45 @@ module.exports = {
 				hostname: 'enjzxtbbcyrptkkutovq.supabase.co'
 			}
 		]
+	},
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://novel-editor-ver2.vercel.app/',
+				permanent: true
+			},
+			{
+				source: '/contact',
+				destination: 'https://novel-editor-ver2.vercel.app/contact',
+				permanent: true
+			},
+			{
+				source: '/drafts',
+				destination: 'https://novel-editor-ver2.vercel.app/drafts',
+				permanent: true
+			},
+			{
+				source: '/error',
+				destination: 'https://novel-editor-ver2.vercel.app/',
+				permanent: true
+			},
+			{
+				source: '/policy',
+				destination: 'https://novel-editor-ver2.vercel.app/policy',
+				permanent: true
+			},
+			{
+				source: '/profile',
+				destination: 'https://novel-editor-ver2.vercel.app/profile',
+				permanent: true
+			},
+			{
+				source: '/thanks',
+				destination: 'https://novel-editor-ver2.vercel.app/',
+				permanent: true
+			}
+			// その他のリダイレクト設定
+		];
 	}
 };
